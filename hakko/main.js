a60369d471502db5e3f7b238e90e6a20dc77bf52
otasora <ootaoota0038@gmail.com>
2026-01-24 22:37:56 +0900

    hakko_ver4追加_模様替え！

--- hakko/main.js
+++ hakko/main.js
@@ -8,4 +8,40 @@ window.onload = function(){
     document.getElementById("setButton3").onclick = function(){
         document.getElementById("cssMain").href = "stylev3.css";
         }
+        document.getElementById("setButton4").onclick = function(){
+        document.getElementById("cssMain").href = "stylev4.css";
+        }
     }
+
+
+const cursor = document.getElementById("custom-cursor");
+
+// モバイルは無効
+if (window.matchMedia("(hover: none)").matches) {
+  // 何もしない
+} else {
+
+  document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top  = e.clientY + "px";
+  });
+
+  document.addEventListener("mouseover", (e) => {
+    if (e.target.closest("a.memolink")) {
+      document.body.classList.add("cursor-memo");
+    }
+    if (e.target.closest("a.otasoraWeb")) {
+      document.body.classList.add("cursor-otasora");
+    }
+  });
+
+  document.addEventListener("mouseout", (e) => {
+    if (e.target.closest("a.memolink")) {
+      document.body.classList.remove("cursor-memo");
+    }
+    if (e.target.closest("a.otasoraWeb")) {
+      document.body.classList.remove("cursor-otasora");
+    }
+  });
+
+}
